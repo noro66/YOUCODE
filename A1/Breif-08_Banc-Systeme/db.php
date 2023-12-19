3c8bb35f5a7e9ef3c672af408e934aac04a18b7f
Nouaamane <noro6@gmail.com>
2023-12-19 21:36:07 +0100

    completing the crud functions for the user table

--- A1/Breif-08_Banc-Systeme/db.php
+++ A1/Breif-08_Banc-Systeme/db.php
@@ -49,9 +49,64 @@ class Database
             return false;
         }
     }
+
+    public function getUserById($id)
+    {
+
+
+        try {
+            $sql = " SELECT * FROM users WHERE id = :id";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute(['id' => $id]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            echo "Error : " . $e->getMessage();
+        }
+    }
+
+    public function update($id, $fname, $lname, $email, $phone)
+    {
+        try {
+            $sql = "UPDATE users SET first_name = :fname, last_name = :lname, email = :email, phone = :phone WHERE id = :id";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute(['id' => $id, 'fname' => $fname, 'lname' => $lname, 'email' => $email, 'phone' => $phone]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Error : " . $e->getMessage();
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $sql = "DELETE FROM users WHERE id = :id ";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute(['id' => $id]);
+            return true;
+        } catch (PDOException $e) {
+            echo "Error : " . $e->getMessage();
+        }
+    }
+    public function totalRowcount()
+    {
+        try {
+            $sql = "SELECT * FROM users";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute();
+            $t_rows = $stmt->rowCount();
+            return $t_rows;
+        } catch (PDOException $e) {
+            echo "Error : " . $e->getMessage();
+        }
+    }
 }
+
 
 $do = new Database();
 echo "<pre>";
 print_r($do->read());
 echo "<pre>";
+
+echo "<br>";
+echo $do->totalRowcount();
