e09e886c9f88599f9e8b65f30e2fc6026c67ff29
Nouaamane <noro6@gmail.com>
2024-01-08 12:23:32 +0100

    login page

--- A1/Brief-11_wikis/app/core/config.php
+++ A1/Brief-11_wikis/app/core/config.php
@@ -3,4 +3,4 @@ define("ROOT", 'http://localhost/YOUCODE/A1/Brief-11_wikis/public/');
 define("DBNAME", "testdata");
 define("DBHOST", "localhost");
 define("DBUSER", "root");
-define("DBPASS", "");
+define("APP_NAME", "WIKIS");
--- A1/Brief-11_wikis/app/views/home.view.php
+++ A1/Brief-11_wikis/app/views/home.view.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="Nouaamane Aitsfia">
     <meta name="generator" content="Hugo 0.118.2">
-    <title>Wikis</title>
+    <title><?= APP_NAME ?></title>
     <link href="<?= ROOT ?>assets/css/bootstrap.min.css" rel="stylesheet">
 
 
@@ -85,7 +85,7 @@
                 <h3 class="float-md-start mb-0">Cover</h3>
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
+                    <a class="nav-link fw-bold py-1 px-0" href="<?= ROOT . 'login' ?>">login</a>
                     <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
                 </nav>
             </div>
@@ -100,10 +100,9 @@
         </main>
 
         <footer class="mt-auto text-white-50">
-            <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
         </footer>
     </div>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<? ROOT ?>assets/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
