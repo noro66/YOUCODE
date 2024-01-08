0e53b5d4ac78714d3746af283df21c806646df44
Nouaamane <noro6@gmail.com>
2024-01-08 21:35:20 +0100

    login

--- A1/Brief-11_wikis/app/controllers/Home.php
+++ A1/Brief-11_wikis/app/controllers/Home.php
@@ -4,7 +4,7 @@ class Home
     use  Controller;
     public function index(...$arr)
     {
-        $data['email'] = empty($_SESSION['USER']) ? 'NO USER' : $_SESSION['USER'];
+        $data['email'] = empty($_SESSION['USER']->email) ? 'NO USER' : $_SESSION['USER']->email;
         $this->view('home', $data);
     }
 }
