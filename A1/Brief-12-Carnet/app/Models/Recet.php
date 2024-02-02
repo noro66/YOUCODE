cf19b6fcf103b36765452af5126abd8a53ac249f
Mogy <norou6@gmail.com>
2024-02-02 10:31:45 +0100

    recet

--- A1/Brief-12-Carnet/app/Models/Recet.php
+++ A1/Brief-12-Carnet/app/Models/Recet.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use mysql_xdevapi\Table;
 
 class Recet extends Model
 {
     use HasFactory;
+    protected  $table = 'recet';
 }
