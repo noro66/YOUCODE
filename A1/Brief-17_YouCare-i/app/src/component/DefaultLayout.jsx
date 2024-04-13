fe14a06d47fec886a96eaa3e350e147e74b3cfc0
Mogy <norou6@gmail.com>
2024-04-13 05:34:19 +0000

    logout0

--- A1/Brief-17_YouCare-i/app/src/component/DefaultLayout.jsx
+++ A1/Brief-17_YouCare-i/app/src/component/DefaultLayout.jsx
@@ -1,11 +1,22 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../Context/ContextProvider.jsx";
+import axiosClient from "../axiosClient.js";
 
 function DefaultLayout () {
+    const {setToken, setUser} = useStateContext()
 const { user ,token} = useStateContext();
     if (!token){
         return <Navigate to={'/login'} />;
     }
+
+    function onLogout(e) {
+            e.preventDefault();
+            axiosClient.get("auth/logout", {token: token}).then(({data}) =>  {
+                setToken('');
+                setUser('');
+            }).catch(r => console.log(r));
+    }
+
     return (
         <div id='defaultLayout'>
             <div className={'content'}>
@@ -14,7 +25,8 @@ const { user ,token} = useStateContext();
                         header
                     </div>
                     <div>
-                        {user.name}
+                        {user && user.name}
+                        <a onClick={onLogout} className={'btn-logout cursor-pointer'}>Logout</a>
                     </div>
                 </header>
                 <main>
